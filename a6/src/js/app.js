c7530e8626fdf223977566032f33ffc431ce3e7e
Nathan Kong <mhk.nathan@gmail.com>
2024-04-09 15:39:21 -0400

    AS6 Init

---
+++ a6/src/js/app.js
@@ -0,0 +1,135 @@
+/**
+ * WEB222 – Assignment 06
+ *
+ * I declare that this assignment is my own work in accordance with
+ * Seneca Academic Policy. No part of this assignment has been
+ * copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Please update the following with your information:
+ *
+ *      Name:       Nathan Kong
+ *      Student ID: 150950236
+ *      Date:       2024-04-09
+ */
+
+// All of our data is available on the global `window` object.
+// Create local variables to work with it in this file.
+const { artists, songs } = window;
+
+function createAnchor(href, textContent) {
+  const anchor = document.createElement("a");
+
+  anchor.setAttribute("href", href);
+  anchor.setAttribute("target", "_blank");
+  anchor.textContent = textContent;
+
+  return anchor;
+}
+
+function updateLinks(artist) {
+  const { name, urls, region } = artist;
+  const selection = document.getElementById("selected-artist");
+
+  // Selected artist
+  selection.textContent = `${name} | ${region}`;
+  selection.insertAdjacentElement("beforeend", document.createElement("br"));
+
+  // Create the links
+  for (let i = 0; i < urls.length; ++i) {
+    const link = urls[i];
+
+    selection.insertAdjacentElement("beforeend", createAnchor(link.url, link.name));
+
+    if (i < urls.length - 1) {
+      selection.insertAdjacentText("beforeend", " | ");
+    }
+  }
+}
+
+function createCard(song) {
+  const { url, imageUrl, title, year, duration } = song;
+
+  // <div> Card
+  const card = document.createElement("div");
+  card.className = "card";
+
+  // <a><img></a> Clickable Image
+  const img = document.createElement("img");
+  img.setAttribute("src", imageUrl);
+
+  const imgLink = createAnchor(url, "");
+  imgLink.appendChild(img);
+
+  card.appendChild(imgLink);
+
+  // <h3> Song Name
+  const songName = document.createElement("h3");
+  songName.textContent = title;
+
+  card.appendChild(songName);
+
+  // <div> Year - Duration flex box
+  const yearDuration = document.createElement("div");
+
+  // <time> Year
+  const songYear = document.createElement("time");
+  songYear.textContent = year;
+
+  yearDuration.appendChild(songYear);
+
+  // <p> Duration
+  const songDuration = document.createElement("p");
+  const secs = duration;
+  songDuration.textContent = `${parseInt(secs / 60)}:${(secs % 60).toString().padStart(2, "0")}`;
+
+  yearDuration.appendChild(songDuration);
+
+  card.appendChild(yearDuration);
+
+  return card;
+}
+
+function updateSongs({ artistId }) {
+  // Cards container
+  const cards = document.getElementById("cards");
+  cards.innerHTML = "";
+
+  // Filter songs
+  const filteredSongs = songs.filter((song) => {
+    return !song.explicit && song.artistId === artistId;
+  });
+
+  for (const song of filteredSongs) {
+    // Append the card to the cards container
+    cards.appendChild(createCard(song));
+  }
+}
+
+function createArtistButtons(artist) {
+  const button = document.createElement("button");
+
+  button.textContent = artist.name;
+
+  button.onclick = () => {
+    updateLinks(artist);
+    updateSongs(artist);
+  };
+
+  return button;
+}
+
+addEventListener("DOMContentLoaded", () => {
+  const menu = document.getElementById("menu");
+  let first = true;
+
+  artists.forEach((artist) => {
+    const button = createArtistButtons(artist);
+    menu.appendChild(button);
+
+    if (first) {
+      first = false;
+      button.click();
+    }
+  });
+});
---
+++ a6/src/js/songs.js
@@ -0,0 +1,278 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+window.songs = [
+  {
+    songId: "SID-00",
+    artistId: "AID-00",
+    title: "駱駝",
+    year: "2023",
+    duration: 246,
+    url: "https://www.youtube.com/watch?v=b13zt7RGr10",
+    imageUrl: "https://img.youtube.com/vi/b13zt7RGr10/hqdefault.jpg",
+    explicit: true
+  },
+  {
+    songId: "SID-01",
+    artistId: "AID-00",
+    title: "麥記最後一夜",
+    year: "2016",
+    duration: 206,
+    url: "https://www.youtube.com/watch?v=gmmifg47NCA",
+    imageUrl: "https://img.youtube.com/vi/gmmifg47NCA/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-02",
+    artistId: "AID-00",
+    title: "那陣時不知道",
+    year: "2021",
+    duration: 147,
+    url: "https://www.youtube.com/watch?v=5xi49OreS3k",
+    imageUrl: "https://img.youtube.com/vi/5xi49OreS3k/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-03",
+    artistId: "AID-00",
+    title: "詩歌舞街",
+    year: "2021",
+    duration: 191,
+    url: "https://www.youtube.com/watch?v=J2DxGXp8KYY",
+    imageUrl: "https://img.youtube.com/vi/J2DxGXp8KYY/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-04",
+    artistId: "AID-00",
+    title: "美麗新香港",
+    year: "2014",
+    duration: 117,
+    url: "https://www.youtube.com/watch?v=S04c7OBzzhM",
+    imageUrl: "https://img.youtube.com/vi/S04c7OBzzhM/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-05",
+    artistId: "AID-00",
+    title: "今夜雪糕",
+    year: "2021",
+    duration: 147,
+    url: "https://www.youtube.com/watch?v=SFcFcvD7CDA",
+    imageUrl: "https://img.youtube.com/vi/SFcFcvD7CDA/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-06",
+    artistId: "AID-00",
+    title: "吳小姐",
+    year: 2019,
+    duration: 185,
+    url: "https://www.youtube.com/watch?v=B-I2hbclzXc",
+    imageUrl: "https://img.youtube.com/vi/B-I2hbclzXc/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-07",
+    artistId: "AID-00",
+    title: "K同學",
+    year: "2021",
+    duration: 180,
+    url: "https://www.youtube.com/watch?v=-awnwjlFLfw",
+    imageUrl: "https://img.youtube.com/vi/-awnwjlFLfw/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-08",
+    artistId: "AID-00",
+    title: "今宵多珍重",
+    year: 2019,
+    duration: 161,
+    url: "https://www.youtube.com/watch?v=bhhQ9ugtRtA",
+    imageUrl: "https://img.youtube.com/vi/bhhQ9ugtRtA/hqdefault.jpg",
+    explicit: true
+  },
+  {
+    songId: "SID-09",
+    artistId: "AID-01",
+    title: "Strong",
+    year: 2013,
+    duration: 265,
+    url: "https://www.youtube.com/watch?v=6drfp_3823I",
+    imageUrl: "https://img.youtube.com/vi/6drfp_3823I/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-10",
+    artistId: "AID-01",
+    title: "Nightcall",
+    year: 2013,
+    duration: 270,
+    url: "https://www.youtube.com/watch?v=OZYw0MQp_fI",
+    imageUrl: "https://img.youtube.com/vi/OZYw0MQp_fI/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-11",
+    artistId: "AID-01",
+    title: "Hey Now",
+    year: 2013,
+    duration: 207,
+    url: "https://www.youtube.com/watch?v=nMEHJPuggHQ",
+    imageUrl: "https://img.youtube.com/vi/nMEHJPuggHQ/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-12",
+    artistId: "AID-01",
+    title: "Wasting My Young Years",
+    year: 2013,
+    duration: 204,
+    url: "https://www.youtube.com/watch?v=xxTD_lEiQxI",
+    imageUrl: "https://img.youtube.com/vi/xxTD_lEiQxI/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-13",
+    artistId: "AID-01",
+    title: "Non Believer",
+    year: 2017,
+    duration: 257,
+    url: "https://www.youtube.com/watch?v=DC7sHxzRQAA",
+    imageUrl: "https://img.youtube.com/vi/DC7sHxzRQAA/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-14",
+    artistId: "AID-01",
+    title: "Rooting For You",
+    year: 2017,
+    duration: 269,
+    url: "https://www.youtube.com/watch?v=jqhgXAGP4Ho",
+    imageUrl: "https://img.youtube.com/vi/jqhgXAGP4Ho/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-15",
+    artistId: "AID-01",
+    title: "Oh Woman Oh Man",
+    year: 2017,
+    duration: 277,
+    url: "https://www.youtube.com/watch?v=oVQqmH4ufCQ",
+    imageUrl: "https://img.youtube.com/vi/oVQqmH4ufCQ/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-16",
+    artistId: "AID-01",
+    title: "Baby It's You",
+    year: 2021,
+    duration: 243,
+    url: "https://www.youtube.com/watch?v=BlK9a-ccrLY",
+    imageUrl: "https://img.youtube.com/vi/BlK9a-ccrLY/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-17",
+    artistId: "AID-01",
+    title: "Californian Soil",
+    year: 2021,
+    duration: 221,
+    url: "https://www.youtube.com/watch?v=Pbwaq0E80hQ",
+    imageUrl: "https://img.youtube.com/vi/Pbwaq0E80hQ/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-18",
+    artistId: "AID-02",
+    title: "Show Me How",
+    year: 2018,
+    duration: 215,
+    url: "https://www.youtube.com/watch?v=OZRYzH0Q0pU",
+    imageUrl: "https://img.youtube.com/vi/OZRYzH0Q0pU/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-19",
+    artistId: "AID-02",
+    title: "Numb",
+    year: 2019,
+    duration: 219,
+    url: "https://www.youtube.com/watch?v=xAz_DzPUjrM",
+    imageUrl: "https://img.youtube.com/vi/xAz_DzPUjrM/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-20",
+    artistId: "AID-02",
+    title: "Lauren",
+    year: 2016,
+    duration: 218,
+    url: "https://www.youtube.com/watch?v=TNsSBhl_2LI",
+    imageUrl: "https://img.youtube.com/vi/TNsSBhl_2LI/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-21",
+    artistId: "AID-02",
+    title: "I hope to Be Around",
+    year: 2017,
+    duration: 205,
+    url: "https://www.youtube.com/watch?v=DviID8Ni7Ns",
+    imageUrl: "https://img.youtube.com/vi/DviID8Ni7Ns/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-22",
+    artistId: "AID-02",
+    title: "Tailwhip",
+    year: 2017,
+    duration: 225,
+    url: "https://www.youtube.com/watch?v=9IZKcb3LndA",
+    imageUrl: "https://img.youtube.com/vi/9IZKcb3LndA/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-23",
+    artistId: "AID-02",
+    title: "Tree Among Shrubs",
+    year: 2021,
+    duration: 188,
+    url: "https://www.youtube.com/watch?v=0LMwgWFzDjU",
+    imageUrl: "https://img.youtube.com/vi/0LMwgWFzDjU/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-24",
+    artistId: "AID-02",
+    title: "Sugar",
+    year: 2021,
+    duration: 176,
+    url: "https://www.youtube.com/watch?v=93rM4qnUyGA",
+    imageUrl: "https://img.youtube.com/vi/93rM4qnUyGA/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-25",
+    artistId: "AID-02",
+    title: "Seven",
+    year: 2018,
+    duration: 225,
+    url: "https://www.youtube.com/watch?v=pnY7m5CK-as",
+    imageUrl: "https://img.youtube.com/vi/pnY7m5CK-as/hqdefault.jpg",
+    explicit: false
+  },
+  {
+    songId: "SID-26",
+    artistId: "AID-02",
+    title: "Billie Toppy",
+    year: 2022,
+    duration: 195,
+    url: "https://www.youtube.com/watch?v=HAK5D3drObI",
+    imageUrl: "https://img.youtube.com/vi/HAK5D3drObI/hqdefault.jpg",
+    explicit: false
+  }
+];
